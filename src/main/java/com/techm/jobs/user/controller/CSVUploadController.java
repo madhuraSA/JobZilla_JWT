dc60ca32d848beda3f9467645de9bc13f22011f6
kishoreReddy <kishoreReddy@lokeshs-mbp>
2020-08-17 22:07:35 +0530

    added MediaType in csvUpload

--- src/main/java/com/techm/jobs/user/controller/CSVUploadController.java
+++ src/main/java/com/techm/jobs/user/controller/CSVUploadController.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -35,7 +37,9 @@ public class CSVUploadController {
 	@ApiResponses(value = { @ApiResponse(code = 200, message = "OK"), @ApiResponse(code = 201, message = "Created"),
 	@ApiResponse(code = 401, message = "Unauthorized"), @ApiResponse(code = 403, message = "Forbidden"),
 	@ApiResponse(code = 404, message = "Not Found"), @ApiResponse(code = 405, message = "Invalid input") })
-    @PostMapping("/uploadcsv")
+    //@PostMapping("/uploadcsv")
+	@RequestMapping(value = "/uploadcsv", method = RequestMethod.POST,
+    consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 	 public ResponseEntity<ResponseMessage> uploadFile(@RequestParam("file") MultipartFile file) {
 	    String message = "";
 
